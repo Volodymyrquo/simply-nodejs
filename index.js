7247527b9ffa26745b5efd3e3b571900cf25aa57
Volodymyr <volodymyrquo@gmail.com>
2020-12-20 08:42:51 +0200

    server created

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const chalk = require('chalk');
-const text = require('./data');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-console.log(chalk.blue('Hello NodeJS'));
-console.log(__filename);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  console.log(req.url);
+  res.end('<h1>Hello NodeJS!!!</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('Server has been started...');
+});

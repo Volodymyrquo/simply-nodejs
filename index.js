9886ae59a1d755cfdce20cdf7d926363392a1e7e
Volodymyr <volodymyrquo@gmail.com>
2020-12-20 09:01:36 +0200

    created working site

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  console.log(req.url);
-  res.end('<h1>Hello NodeJS!!!</h1>');
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      });
+      res.end(data);
+    });
+  } else if (req.url === '/contact') {
+    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      });
+      res.end(data);
+    });
+  }
 });
 
 server.listen(3000, () => {

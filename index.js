a53125dbc51a0e7605422806a8b23c18f9fe283c
Volodymyr <volodymyrquo@gmail.com>
2020-12-20 12:00:10 +0200

    finished

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { runInNewContext } = require('vm');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+  /*   if (req.url === '/') {
     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
       if (err) {
         throw err;
@@ -24,8 +25,53 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   }
+ */
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  if (!ext) {
+    filePath += '.html';
+  }
+  console.log(filePath);
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.end('Error');
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html',
+          });
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(200, {
+        'Content-Type': contentType,
+      });
+      res.end(content);
+    }
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server has been started...');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}...`);
 });
